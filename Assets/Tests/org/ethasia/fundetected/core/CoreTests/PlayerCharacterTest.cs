77e99aa8675e604a9cbc4e72503922bd4ed0b88b
Boris Ivanovic <shmuel.yosef.shekelfeld@gmail.com>
2023-10-23 20:07:57 +0200

    add unit tests for player left right movement

---
+++ Assets/Tests/org/ethasia/fundetected/core/CoreTests/PlayerCharacterTest.cs
@@ -0,0 +1,66 @@
+using NUnit.Framework;
+
+using Org.Ethasia.Fundetected.Ioadapters.Mocks;
+
+namespace Org.Ethasia.Fundetected.Core.Tests
+{
+
+    public class PlayerCharacterTest
+    {
+        private PlayerCharacter testCandidate;
+        private PlayerCharacterBaseStats testBaseStats;
+
+        [OneTimeSetUp] 
+        public void Init()
+        {
+            IoAdaptersFactoryForCore.SetInstance(new MockedIoAdaptersFactoryForCore());
+
+            testBaseStats = new PlayerCharacterBaseStats.PlayerCharacterBaseStatsBuilder().SetMovementSpeed(150).Build();
+            testBaseStats.DeriveStats();            
+        }
+
+        [Test]
+        public void TestMoveLeftDistanceUnitsAreCalculatedCorrectly()
+        {
+            testCandidate = new PlayerCharacter.PlayerCharacterBuilder()
+                .SetPlayerCharacterBaseStats(testBaseStats)
+                .Build();    
+
+            int unitsMoved = testCandidate.MoveLeft(0.2);
+            Assert.That(unitsMoved, Is.EqualTo(3));  
+        }
+
+        [Test]
+        public void TestMoveLeftMoveTimeIsBelowThreshold()
+        {
+            testCandidate = new PlayerCharacter.PlayerCharacterBuilder()
+                .SetPlayerCharacterBaseStats(testBaseStats)
+                .Build();  
+
+            int unitsMoved = testCandidate.MoveLeft(0.065);
+            Assert.That(unitsMoved, Is.EqualTo(0));  
+        }        
+
+        [Test]
+        public void TestMoveRightDistanceUnitsAreCalculatedCorrectly()
+        {
+            testCandidate = new PlayerCharacter.PlayerCharacterBuilder()
+                .SetPlayerCharacterBaseStats(testBaseStats)
+                .Build();  
+
+            int unitsMoved = testCandidate.MoveRight(0.54);
+            Assert.That(unitsMoved, Is.EqualTo(8)); 
+        }        
+
+        [Test]
+        public void TestMoveRightMoveTimeIsBelowThreshold()
+        {
+            testCandidate = new PlayerCharacter.PlayerCharacterBuilder()
+                .SetPlayerCharacterBaseStats(testBaseStats)
+                .Build();  
+
+            int unitsMoved = testCandidate.MoveRight(0.065);
+            Assert.That(unitsMoved, Is.EqualTo(0)); 
+        }         
+    }
+}
