5efb47762b38045246dd889249a422e0b0256cb6
Boris Ivanovic <shmuel.yosef.shekelfeld@gmail.com>
2023-11-17 21:00:24 +0100

    add equipment slot types

---
+++ Assets/Scripts/org/ethasia/fundetected/core/equipment/EquipmentSlotTypes.cs
@@ -0,0 +1,16 @@
+namespace Org.Ethasia.Fundetected.Core.Equipment
+{
+
+    public enum EquipmentSlotTypes
+    {
+        MAIN_HAND,
+        OFF_HAND,
+        HEAD,
+        CHEST,
+        HANDS,
+        FEET,
+        BELT,
+        RINGS,
+        AMULET
+    }
+}
