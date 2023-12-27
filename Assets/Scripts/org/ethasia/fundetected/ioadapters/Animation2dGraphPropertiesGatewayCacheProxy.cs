d2ea71d8101623556fd46600a22368d8840fa895
Boris Ivanovic <shmuel.yosef.shekelfeld@gmail.com>
2023-12-27 23:12:48 +0100

    add caching proxy for animation 2d graph properties gateway

---
+++ Assets/Scripts/org/ethasia/fundetected/ioadapters/Animation2dGraphPropertiesGatewayCacheProxy.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+
+namespace Org.Ethasia.Fundetected.Ioadapters
+{
+    public class Animation2dGraphPropertiesGatewayCacheProxy : Animation2dGraphPropertiesGateway
+    {
+        private Animation2dGraphPropertiesGateway realGateway;
+        private Dictionary<string, Animation2dGraphNodeProperties> cache;
+
+        public Animation2dGraphPropertiesGatewayCacheProxy()
+        {
+            realGateway = new Animation2dGraphPropertiesGatewayImpl();
+            cache = new Dictionary<string, Animation2dGraphNodeProperties>();
+        }
+
+        public Animation2dGraphNodeProperties LoadAnimation2dGraph(string animationGraphName)
+        {
+            if (cache.ContainsKey(animationGraphName))
+            {
+                return cache[animationGraphName];
+            }
+
+            cache[animationGraphName] = realGateway.LoadAnimation2dGraph(animationGraphName);
+            return cache[animationGraphName];
+        }        
+    }
+}
