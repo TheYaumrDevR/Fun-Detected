429faf9f64a7b75672df6993e33e203f4f9cc4d6
Fara On <drawig@gmail.com>
2024-01-17 21:26:54 +0100

    add todo note

--- Assets/Scripts/org/ethasia/fundetected/core/equipment/Armor.cs
+++ Assets/Scripts/org/ethasia/fundetected/core/equipment/Armor.cs
@@ -2,6 +2,7 @@ namespace Org.Ethasia.Fundetected.Core.Equipment
 {
     public class Armor : Equipment
     {
+        // TODO
         // Plate Armor types give a lot of armor, but reduce movement speed
         // Leather / Synthetic Armor types give less armor, but add movement speed. They can also have effects like "Ground effects have a chance to skip a damage tick"
         // Cloth Armor types give even less armor, but can give elemental resistances. Or Maximum ele res. Or can reserve flat mana to give additional armor.
