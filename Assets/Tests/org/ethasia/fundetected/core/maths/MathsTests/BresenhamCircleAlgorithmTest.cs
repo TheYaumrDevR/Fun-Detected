67ceda7b3136a5c33753c2ec27c1190dc4847d74
Fara On <drawig@gmail.com>
2024-04-30 19:53:16 +0200

    add another unit test for arc drawing algorithm

--- Assets/Tests/org/ethasia/fundetected/core/maths/MathsTests/BresenhamCircleAlgorithmTest.cs
+++ Assets/Tests/org/ethasia/fundetected/core/maths/MathsTests/BresenhamCircleAlgorithmTest.cs
@@ -123,6 +123,39 @@ namespace Org.Ethasia.Fundetected.Core.Maths.Tests
 
             Assert.That(result[11].X, Is.EqualTo(0)); 
             Assert.That(result[11].Y, Is.EqualTo(1));                                                                                                                
-        }                  
+        }   
+
+        [Test]
+        public void TestCircleArcWithRadiusThreeIsDrawnFromBottomRightToBottomLeft()
+        {
+            BresenhamCircleAlgorithm testCandidate = new BresenhamCircleAlgorithm();
+
+            HitboxTilePosition startPoint = new HitboxTilePosition(5, 5);
+            HitboxTilePosition endPoint = new HitboxTilePosition(0, 4);
+
+            testCandidate.CreateFilledCircleArc(startPoint, endPoint, 3);
+
+            List<HitboxTilePosition> result = testCandidate.HitboxTilePositions;
+            
+            Assert.That(result.Count, Is.EqualTo(7)); 
+
+            Assert.That(result[1].X, Is.EqualTo(5)); 
+            Assert.That(result[1].Y, Is.EqualTo(5));     
+
+            Assert.That(result[2].X, Is.EqualTo(4)); 
+            Assert.That(result[2].Y, Is.EqualTo(6)); 
+
+            Assert.That(result[3].X, Is.EqualTo(3)); 
+            Assert.That(result[3].Y, Is.EqualTo(6)); 
+
+            Assert.That(result[4].X, Is.EqualTo(2)); 
+            Assert.That(result[4].Y, Is.EqualTo(6)); 
+
+            Assert.That(result[5].X, Is.EqualTo(1)); 
+            Assert.That(result[5].Y, Is.EqualTo(5));    
+
+            Assert.That(result[6].X, Is.EqualTo(0)); 
+            Assert.That(result[6].Y, Is.EqualTo(4));                                                                                                              
+        }                        
     }
 }
