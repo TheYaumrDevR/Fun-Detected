3c44975dbb5b8aa232a55f1dfdc6d5f55c139c9d
Fara On <drawig@gmail.com>
2024-05-07 20:25:37 +0200

    fixed unit tests for hit arc generation algorithm

--- Assets/Tests/org/ethasia/fundetected/core/maths/MathsTests/BresenhamBasedHitArcGenerationAlgorithmTest.cs
+++ Assets/Tests/org/ethasia/fundetected/core/maths/MathsTests/BresenhamBasedHitArcGenerationAlgorithmTest.cs
@@ -13,8 +13,8 @@ namespace Org.Ethasia.Fundetected.Core.Maths.Tests
             testCandidate.CreateFilledCircleArc(-1.5708, 0.383972, 2);
 
             List<HitboxTilePosition> result = testCandidate.HitboxTilePositions;
-            
-            Assert.That(result.Count, Is.EqualTo(6)); 
+
+            Assert.That(result.Count, Is.EqualTo(10)); 
 
             Assert.That(result[1].X, Is.EqualTo(2)); 
             Assert.That(result[1].Y, Is.EqualTo(0));     
@@ -29,7 +29,19 @@ namespace Org.Ethasia.Fundetected.Core.Maths.Tests
             Assert.That(result[4].Y, Is.EqualTo(2)); 
 
             Assert.That(result[5].X, Is.EqualTo(4)); 
-            Assert.That(result[5].Y, Is.EqualTo(3));                                                   
+            Assert.That(result[5].Y, Is.EqualTo(3));       
+
+            Assert.That(result[6].X, Is.EqualTo(2)); 
+            Assert.That(result[6].Y, Is.EqualTo(1));   
+
+            Assert.That(result[7].X, Is.EqualTo(3)); 
+            Assert.That(result[7].Y, Is.EqualTo(1));   
+
+            Assert.That(result[8].X, Is.EqualTo(3)); 
+            Assert.That(result[8].Y, Is.EqualTo(2));   
+
+            Assert.That(result[9].X, Is.EqualTo(3)); 
+            Assert.That(result[9].Y, Is.EqualTo(3));                                                      
         }
 
         [Test]
@@ -40,8 +52,8 @@ namespace Org.Ethasia.Fundetected.Core.Maths.Tests
             testCandidate.CreateFilledCircleArc(-1.5708, 2.74017, 2);
 
             List<HitboxTilePosition> result = testCandidate.HitboxTilePositions;
-            
-            Assert.That(result.Count, Is.EqualTo(10)); 
+
+            Assert.That(result.Count, Is.EqualTo(16)); 
 
             Assert.That(result[1].X, Is.EqualTo(2)); 
             Assert.That(result[1].Y, Is.EqualTo(0));     
@@ -68,7 +80,25 @@ namespace Org.Ethasia.Fundetected.Core.Maths.Tests
             Assert.That(result[8].Y, Is.EqualTo(4));       
 
             Assert.That(result[9].X, Is.EqualTo(0)); 
-            Assert.That(result[9].Y, Is.EqualTo(3));                                                                                            
+            Assert.That(result[9].Y, Is.EqualTo(3));           
+
+            Assert.That(result[10].X, Is.EqualTo(2)); 
+            Assert.That(result[10].Y, Is.EqualTo(1));   
+
+            Assert.That(result[11].X, Is.EqualTo(3)); 
+            Assert.That(result[11].Y, Is.EqualTo(1));               
+
+            Assert.That(result[12].X, Is.EqualTo(3)); 
+            Assert.That(result[12].Y, Is.EqualTo(2));   
+
+            Assert.That(result[13].X, Is.EqualTo(3)); 
+            Assert.That(result[13].Y, Is.EqualTo(3));   
+
+            Assert.That(result[14].X, Is.EqualTo(2)); 
+            Assert.That(result[14].Y, Is.EqualTo(3));   
+
+            Assert.That(result[15].X, Is.EqualTo(1)); 
+            Assert.That(result[15].Y, Is.EqualTo(3));                                                                                    
         }      
 
         [Test]
@@ -79,8 +109,8 @@ namespace Org.Ethasia.Fundetected.Core.Maths.Tests
             testCandidate.CreateFilledCircleArc(-1.5708, -2.74017, 2);
 
             List<HitboxTilePosition> result = testCandidate.HitboxTilePositions;
-            
-            Assert.That(result.Count, Is.EqualTo(12)); 
+
+            Assert.That(result.Count, Is.EqualTo(20)); 
 
             Assert.That(result[1].X, Is.EqualTo(2)); 
             Assert.That(result[1].Y, Is.EqualTo(0));     
@@ -113,7 +143,31 @@ namespace Org.Ethasia.Fundetected.Core.Maths.Tests
             Assert.That(result[10].Y, Is.EqualTo(2)); 
 
             Assert.That(result[11].X, Is.EqualTo(0)); 
-            Assert.That(result[11].Y, Is.EqualTo(1));                                                                                                                
+            Assert.That(result[11].Y, Is.EqualTo(1));   
+
+            Assert.That(result[12].X, Is.EqualTo(2)); 
+            Assert.That(result[12].Y, Is.EqualTo(1));   
+
+            Assert.That(result[13].X, Is.EqualTo(3)); 
+            Assert.That(result[13].Y, Is.EqualTo(1));   
+
+            Assert.That(result[14].X, Is.EqualTo(3)); 
+            Assert.That(result[14].Y, Is.EqualTo(2));   
+
+            Assert.That(result[15].X, Is.EqualTo(3)); 
+            Assert.That(result[15].Y, Is.EqualTo(3));        
+
+            Assert.That(result[16].X, Is.EqualTo(2)); 
+            Assert.That(result[16].Y, Is.EqualTo(3));   
+
+            Assert.That(result[17].X, Is.EqualTo(1)); 
+            Assert.That(result[17].Y, Is.EqualTo(3));          
+
+            Assert.That(result[18].X, Is.EqualTo(1)); 
+            Assert.That(result[18].Y, Is.EqualTo(2));  
+
+            Assert.That(result[19].X, Is.EqualTo(1)); 
+            Assert.That(result[19].Y, Is.EqualTo(1));                                                                                                                                                              
         }   
 
         [Test]
@@ -124,8 +178,8 @@ namespace Org.Ethasia.Fundetected.Core.Maths.Tests
             testCandidate.CreateFilledCircleArc(0.785398, 2.74017, 3);
 
             List<HitboxTilePosition> result = testCandidate.HitboxTilePositions;
-            
-            Assert.That(result.Count, Is.EqualTo(7)); 
+
+            Assert.That(result.Count, Is.EqualTo(15)); 
 
             Assert.That(result[1].X, Is.EqualTo(5)); 
             Assert.That(result[1].Y, Is.EqualTo(5));     
@@ -143,7 +197,94 @@ namespace Org.Ethasia.Fundetected.Core.Maths.Tests
             Assert.That(result[5].Y, Is.EqualTo(5));    
 
             Assert.That(result[6].X, Is.EqualTo(0)); 
-            Assert.That(result[6].Y, Is.EqualTo(4));                                                                                                              
-        }                     
+            Assert.That(result[6].Y, Is.EqualTo(4));       
+
+            Assert.That(result[7].X, Is.EqualTo(4)); 
+            Assert.That(result[7].Y, Is.EqualTo(4));  
+
+            Assert.That(result[8].X, Is.EqualTo(3)); 
+            Assert.That(result[8].Y, Is.EqualTo(4));       
+
+            Assert.That(result[9].X, Is.EqualTo(4)); 
+            Assert.That(result[9].Y, Is.EqualTo(5));     
+
+            Assert.That(result[10].X, Is.EqualTo(3)); 
+            Assert.That(result[10].Y, Is.EqualTo(5)); 
+
+            Assert.That(result[11].X, Is.EqualTo(2)); 
+            Assert.That(result[11].Y, Is.EqualTo(5));   
+
+            Assert.That(result[12].X, Is.EqualTo(2)); 
+            Assert.That(result[12].Y, Is.EqualTo(4));   
+
+            Assert.That(result[13].X, Is.EqualTo(2)); 
+            Assert.That(result[13].Y, Is.EqualTo(3));                                                                                                                                
+        }  
+
+        [Test]
+        public void TestCircleArcWithRadiusFourIsDrawnFromBottomRightToBottomLeft()
+        {
+            BresenhamBasedHitArcGenerationAlgorithm testCandidate = new BresenhamBasedHitArcGenerationAlgorithm();
+
+            testCandidate.CreateFilledCircleArc(1.22173, 2.79253, 4);
+
+            List<HitboxTilePosition> result = testCandidate.HitboxTilePositions;
+
+            Assert.That(result.Count, Is.EqualTo(18)); 
+
+            Assert.That(result[0].X, Is.EqualTo(4)); 
+            Assert.That(result[0].Y, Is.EqualTo(4));  
+
+            Assert.That(result[1].X, Is.EqualTo(5)); 
+            Assert.That(result[1].Y, Is.EqualTo(8));     
+
+            Assert.That(result[2].X, Is.EqualTo(4)); 
+            Assert.That(result[2].Y, Is.EqualTo(8)); 
+
+            Assert.That(result[3].X, Is.EqualTo(3)); 
+            Assert.That(result[3].Y, Is.EqualTo(8)); 
+
+            Assert.That(result[4].X, Is.EqualTo(2)); 
+            Assert.That(result[4].Y, Is.EqualTo(7)); 
+
+            Assert.That(result[5].X, Is.EqualTo(1)); 
+            Assert.That(result[5].Y, Is.EqualTo(6));    
+
+            Assert.That(result[6].X, Is.EqualTo(0)); 
+            Assert.That(result[6].Y, Is.EqualTo(5));       
+
+            Assert.That(result[7].X, Is.EqualTo(4)); 
+            Assert.That(result[7].Y, Is.EqualTo(5));  
+
+            Assert.That(result[8].X, Is.EqualTo(5)); 
+            Assert.That(result[8].Y, Is.EqualTo(6));       
+
+            Assert.That(result[9].X, Is.EqualTo(5)); 
+            Assert.That(result[9].Y, Is.EqualTo(7));     
+
+            Assert.That(result[10].X, Is.EqualTo(4)); 
+            Assert.That(result[10].Y, Is.EqualTo(6)); 
+
+            Assert.That(result[11].X, Is.EqualTo(4)); 
+            Assert.That(result[11].Y, Is.EqualTo(7));   
+
+            Assert.That(result[12].X, Is.EqualTo(3)); 
+            Assert.That(result[12].Y, Is.EqualTo(6));   
+
+            Assert.That(result[13].X, Is.EqualTo(3)); 
+            Assert.That(result[13].Y, Is.EqualTo(7));   
+
+            Assert.That(result[14].X, Is.EqualTo(3)); 
+            Assert.That(result[14].Y, Is.EqualTo(5));     
+
+            Assert.That(result[15].X, Is.EqualTo(2)); 
+            Assert.That(result[15].Y, Is.EqualTo(5));        
+
+            Assert.That(result[16].X, Is.EqualTo(3)); 
+            Assert.That(result[16].Y, Is.EqualTo(4));   
+
+            Assert.That(result[17].X, Is.EqualTo(1)); 
+            Assert.That(result[17].Y, Is.EqualTo(5));                                                                                                                     
+        }                            
     }
 }
