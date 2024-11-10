7d7b6b3ccbf437e84c0260c3b9e586a4f63ed1b7
Fara On <drawig@gmail.com>
2024-11-11 00:07:57 +0100

    add master data for weapon belts

---
+++ Assets/Scripts/org/ethasia/fundetected/ioadapters/JewelryMasterData.cs
@@ -0,0 +1,104 @@
+using Org.Ethasia.Fundetected.Core.Equipment;
+using Org.Ethasia.Fundetected.Core.Equipment.Affixes;
+
+namespace Org.Ethasia.Fundetected.Ioadapters
+{
+    public class JewelryMasterData
+    {
+        public ItemClass ItemClass
+        {
+            get;
+            private set;
+        }
+
+        public string Name
+        {
+            get;
+            private set;
+        }
+
+        public int StrengthRequirement
+        {
+            get;
+            private set;
+        }    
+
+        public int AgilityRequirement
+        {
+            get;
+            private set;
+        }       
+
+        public int IntelligenceRequirement
+        {
+            get;
+            private set;
+        }  
+
+        public EquipmentAffix FirstImplicit
+        {
+            get;
+            private set;
+        }
+
+        public class Builder
+        {
+            private ItemClass itemClass;
+            private string name;
+            private int strengthRequirement;
+            private int agilityRequirement;
+            private int intelligenceRequirement;
+            private EquipmentAffix firstImplicit;
+
+            public Builder SteItemClass(ItemClass value)
+            {
+                itemClass = value;
+                return this;
+            }
+
+            public Builder SetName(string value)
+            {
+                name = value;
+                return this;
+            }
+
+            public Builder SetStrengthRequirement(int value)
+            {
+                strengthRequirement = value;
+                return this;
+            }
+
+            public Builder SetAgilityRequirement(int value)
+            {
+                agilityRequirement = value;
+                return this;
+            }
+
+            public Builder SetIntelligenceRequirement(int value)
+            {
+                intelligenceRequirement = value;
+                return this;
+            }            
+
+            public Builder SetFirstImplicit(EquipmentAffix value)
+            {
+                firstImplicit = value;
+                return this;
+            }
+
+            public JewelryMasterData Build()
+            {
+                JewelryMasterData result = new JewelryMasterData();
+                
+                result.ItemClass = itemClass;
+                result.Name = name;
+                result.StrengthRequirement = strengthRequirement;
+                result.AgilityRequirement = agilityRequirement;
+                result.IntelligenceRequirement = intelligenceRequirement;
+                result.FirstImplicit = firstImplicit;
+
+                return result;
+            }
+        }        
+    }
+}
---
+++ Assets/Scripts/org/ethasia/fundetected/ioadapters/JewelryMasterDataProvider.cs
@@ -0,0 +1,17 @@
+using Org.Ethasia.Fundetected.Core.Equipment;
+using Org.Ethasia.Fundetected.Core.Equipment.Affixes;
+
+namespace Org.Ethasia.Fundetected.Ioadapters
+{
+    public class JewelryMasterDataProvider
+    {
+        public JewelryMasterData GetWeaponsBeltMasterData()
+        {
+            return new JewelryMasterData.Builder()
+                .SetName("Weapons Belt")
+                .SteItemClass(ItemClass.BELTS)
+                .SetFirstImplicit(new PlusStrengthAffix(30))
+                .Build();
+        }
+    }
+}
