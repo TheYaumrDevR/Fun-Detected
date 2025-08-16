db8e7c2f7677a661b872565e76f75da417af431a
Faraon <phah.ra.on@gmail.com>
2025-08-17 01:03:43 +0200

    add Iron Amulet, War Belt and Diamond Band to global drop table

--- Assets/Scripts/org/ethasia/fundetected/ioadapters/DropTableMasterDataProvider.cs
+++ Assets/Scripts/org/ethasia/fundetected/ioadapters/DropTableMasterDataProvider.cs
@@ -5,18 +5,26 @@ namespace Org.Ethasia.Fundetected.Ioadapters
     public class DropTableMasterDataProvider
     {
         private PotionMasterDataProvider potionMasterDataProvider;
+        private JewelryMasterDataProvider jewelryMasterDataProvider;
 
         public DropTableMasterDataProvider()
         {
             this.potionMasterDataProvider = new PotionMasterDataProvider();
+            this.jewelryMasterDataProvider = new JewelryMasterDataProvider();
         }
 
         public DropTableMasterData GetGlobalDropTable()
         {
-            DropTableEntryMasterData tinyLifePotion = new DropTableEntryMasterData(1.0, potionMasterDataProvider.GetTinyLifePotionMasterData());
+            DropTableEntryMasterData tinyLifePotion = new DropTableEntryMasterData(0.25, potionMasterDataProvider.GetTinyLifePotionMasterData());
+            DropTableEntryMasterData warBelt = new DropTableEntryMasterData(0.25, jewelryMasterDataProvider.GetWarBeltMasterData());
+            DropTableEntryMasterData ironAmulet = new DropTableEntryMasterData(0.25, jewelryMasterDataProvider.GetIronAmuletMasterData());
+            DropTableEntryMasterData ironBand = new DropTableEntryMasterData(0.25, jewelryMasterDataProvider.GetDiamondBandMasterData());
 
             DropTableRowMasterData tier1Row = new DropTableRowMasterData(1.0);
             tier1Row.DropTableEntries.Add(tinyLifePotion);
+            tier1Row.DropTableEntries.Add(warBelt);
+            tier1Row.DropTableEntries.Add(ironAmulet);
+            tier1Row.DropTableEntries.Add(ironBand);
 
             DropTableMasterData result = new DropTableMasterData(100);
             result.DropTableRows.Add(tier1Row);
