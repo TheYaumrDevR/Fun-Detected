5535f8c9ca29bbae4d669bef4d4e5875625fafba
Faraon <phah.ra.on@gmail.com>
2025-12-07 18:08:33 +0100

    add unit test to player total stats to make sure that calling calculate twice calculates the same values

--- Assets/Tests/org/ethasia/fundetected/core/map/MapTests/PlayerCharacterTotalStatsTest.cs
+++ Assets/Tests/org/ethasia/fundetected/core/map/MapTests/PlayerCharacterTotalStatsTest.cs
@@ -292,6 +292,140 @@ namespace Org.Ethasia.Fundetected.Core.Map.Tests
             Assert.That(testCandidate.MovementSpeed, Is.EqualTo(423)); 
         }
 
+        [Test]
+        public void TestCalculateCalculatesSameValuesWhenCalculateIsCalledTwice()
+        {
+            PlayerCharacterAdditionalStats statModifiers = CreateAdditionalStats();
+            PlayerCharacterBaseStats baseStats = CreateBaseStats();
+
+            PlayerCharacterTotalStats testCandidate = new PlayerCharacterTotalStats();
+
+            testCandidate.Calculate(baseStats, statModifiers);
+            testCandidate.Calculate(baseStats, statModifiers);
+
+            Assert.That(testCandidate.Intelligence, Is.EqualTo(177));
+            Assert.That(testCandidate.Agility, Is.EqualTo(96));
+            Assert.That(testCandidate.Strength, Is.EqualTo(250));
+            Assert.That(testCandidate.MaximumLife, Is.EqualTo(2417));
+            Assert.That(testCandidate.MaximumMana, Is.EqualTo(794));
+            Assert.That(testCandidate.PhysicalDamageWithMeleeAttacks.MinDamage, Is.EqualTo(184));
+            Assert.That(testCandidate.PhysicalDamageWithMeleeAttacks.MaxDamage, Is.EqualTo(301));
+            Assert.That(testCandidate.PhysicalDamageWithRangedAttacks.MinDamage, Is.EqualTo(63));
+            Assert.That(testCandidate.PhysicalDamageWithRangedAttacks.MaxDamage, Is.EqualTo(141));
+            Assert.That(testCandidate.PhysicalDamageWithSpells.MinDamage, Is.EqualTo(109));
+            Assert.That(testCandidate.PhysicalDamageWithSpells.MaxDamage, Is.EqualTo(160));
+            Assert.That(testCandidate.AccuracyRating, Is.EqualTo(642));
+            Assert.That(testCandidate.EvasionRating, Is.EqualTo(726));
+            Assert.That(testCandidate.AttacksPerSecond, Is.EqualTo(3.0576001495361336));
+            Assert.That(testCandidate.MovementSpeed, Is.EqualTo(423));
+        }
+
+        private PlayerCharacterAdditionalStats CreateAdditionalStats()
+        {
+            PlayerCharacterAdditionalStats result = new PlayerCharacterAdditionalStats();
+
+            result.AddIntelligenceAddend(3);
+            result.AddIntelligenceAddend(9);
+
+            result.AddIntelligenceIncrease(0.2f);
+            result.AddIntelligenceIncrease(0.4f);
+
+            result.AddIntelligenceMultiplier(1.3f);
+            result.AddIntelligenceMultiplier(1.5f);
+
+            result.AddAgilityAddend(2);
+            result.AddAgilityAddend(5);
+
+            result.AddAgilityIncrease(0.1f);
+            result.AddAgilityIncrease(0.3f);
+
+            result.AddAgilityMultiplier(1.2f);
+            result.AddAgilityMultiplier(1.4f);
+
+            result.AddStrengthAddend(4);
+            result.AddStrengthAddend(8);
+
+            result.AddStrengthIncrease(0.3f);
+            result.AddStrengthIncrease(0.5f);
+
+            result.AddStrengthMultiplier(1.1f);
+            result.AddStrengthMultiplier(1.6f);
+
+            result.AddMaximumLifeAddend(50);
+            result.AddMaximumLifeAddend(100);
+
+            result.AddMaximumLifeIncrease(0.2f);
+            result.AddMaximumLifeIncrease(0.4f);
+
+            result.AddMaximumLifeMultiplier(1.3f);
+            result.AddMaximumLifeMultiplier(1.5f);
+
+            result.AddMaximumManaAddend(20);
+            result.AddMaximumManaAddend(30);
+
+            result.AddMaximumManaIncrease(0.1f);
+            result.AddMaximumManaIncrease(0.3f);
+
+            result.AddMaximumManaMultiplier(1.2f);
+            result.AddMaximumManaMultiplier(1.4f);
+
+            result.AddAddedPhysicalDamage(new DamageRange(5, 10));
+            result.AddAddedPhysicalDamage(new DamageRange(7, 15));
+
+            result.AddAddedPhysicalDamageIncrease(0.2f);
+            result.AddAddedPhysicalDamageIncrease(0.4f);
+
+            result.AddAddedPhysicalDamageMultiplier(1.3f);
+            result.AddAddedPhysicalDamageMultiplier(1.5f);
+
+            result.AddAddedPhysicalDamageWithMeleeAttacks(new DamageRange(18, 19));
+            result.AddAddedPhysicalDamageWithMeleeAttacksIncrease(0.1f);
+            result.AddAddedPhysicalDamageWithMeleeAttacksMultiplier(1.05f);
+
+            result.AddAddedPhysicalDamageWithRangedAttacks(new DamageRange(6, 15));
+            result.AddAddedPhysicalDamageWithRangedAttacksIncrease(0.05f);
+            result.AddAddedPhysicalDamageWithRangedAttacksMultiplier(1.1f);
+
+            result.AddAddedPhysicalDamageWithSpells(new DamageRange(18, 19));
+            result.AddAddedPhysicalDamageWithSpellsIncrease(0.07f);
+            result.AddAddedPhysicalDamageWithSpellsMultiplier(1.12f);
+
+            result.AddAccuracyRatingAddend(10);
+            result.AddAccuracyRatingAddend(20);
+
+            result.AddAccuracyRatingIncrease(0.1f);
+            result.AddAccuracyRatingIncrease(0.3f);
+
+            result.AddAccuracyRatingMultiplier(1.2f);
+            result.AddAccuracyRatingMultiplier(1.4f);
+
+            result.AddEvasionRatingAddend(15);
+            result.AddEvasionRatingAddend(25);
+
+            result.AddEvasionRatingIncrease(0.2f);
+            result.AddEvasionRatingIncrease(0.4f);
+
+            result.AddEvasionRatingMultiplier(1.3f);
+            result.AddEvasionRatingMultiplier(1.5f);
+
+            result.AddAttacksPerSecondIncrease(0.1f);
+            result.AddAttacksPerSecondIncrease(0.2f);
+
+            result.AddAttacksPerSecondMultiplier(1.2f);
+            result.AddAttacksPerSecondMultiplier(1.4f);
+
+            result.AddMovementSpeedAddend(10);
+            result.AddMovementSpeedAddend(20);
+
+            result.AddMovementSpeedIncrease(0.1f);
+            result.AddMovementSpeedIncrease(0.3f);
+
+            result.AddMovementSpeedMultiplier(1.2f);
+            result.AddMovementSpeedMultiplier(1.4f);
+
+            return result;
+        }
+
         private PlayerCharacterBaseStats CreateBaseStats()
         {
             return new PlayerCharacterBaseStats.PlayerCharacterBaseStatsBuilder()
