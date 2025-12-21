44e709f724af4da6fa91fc0087b432c9d244a280
Faraon <phah.ra.on@gmail.com>
2025-12-21 14:13:13 +0100

    add unit tests to StatsFromEquipment for increase and decrease of physical damage

--- Assets/Tests/org/ethasia/fundetected/core/CoreTests/StatsFromEquipmentTest.cs
+++ Assets/Tests/org/ethasia/fundetected/core/CoreTests/StatsFromEquipmentTest.cs
@@ -47,6 +47,70 @@ namespace Org.Ethasia.Fundetected.Core.Tests
             Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithLeftHandMeleeAttacks.MaxDamage, Is.EqualTo(13));            
             Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithRangedAttacks.MinDamage, Is.EqualTo(6));
             Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithRangedAttacks.MaxDamage, Is.EqualTo(13));
-        }        
+        }     
+
+        [Test]   
+        public void TestIncreasePlusMinMaxPhysicalDamageWithMeleeAttacksBy_IncreasesOnlyMeleeAttacks()
+        {
+            // Arrange
+            StatsFromEquipment testCandidate = new StatsFromEquipment();
+            
+            // Verify initial state
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithRightHandMeleeAttacks.MinDamage, Is.EqualTo(0));
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithRightHandMeleeAttacks.MaxDamage, Is.EqualTo(0));
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithLeftHandMeleeAttacks.MinDamage, Is.EqualTo(0));
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithLeftHandMeleeAttacks.MaxDamage, Is.EqualTo(0));
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithRangedAttacks.MinDamage, Is.EqualTo(0));
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithRangedAttacks.MaxDamage, Is.EqualTo(0));
+            
+            // Act
+            testCandidate.IncreasePlusMinMaxPhysicalDamageWithMeleeAttacksBy(4, 10);
+            
+            // Assert
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithRightHandMeleeAttacks.MinDamage, Is.EqualTo(4));
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithRightHandMeleeAttacks.MaxDamage, Is.EqualTo(10));
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithLeftHandMeleeAttacks.MinDamage, Is.EqualTo(4));
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithLeftHandMeleeAttacks.MaxDamage, Is.EqualTo(10));
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithRangedAttacks.MinDamage, Is.EqualTo(0));
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithRangedAttacks.MaxDamage, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestDecreasePlusMinMaxPhysicalDamageWithAttacksBy_DecreasesBothMeleeAndRangedAttacks()
+        {
+            // Arrange
+            StatsFromEquipment testCandidate = new StatsFromEquipment();
+            testCandidate.IncreasePlusMinMaxPhysicalDamageWithAttacksBy(10, 20);
+            
+            // Act
+            testCandidate.DecreasePlusMinMaxPhysicalDamageWithAttacksBy(4, 8);
+            
+            // Assert
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithRightHandMeleeAttacks.MinDamage, Is.EqualTo(6));
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithRightHandMeleeAttacks.MaxDamage, Is.EqualTo(12));
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithLeftHandMeleeAttacks.MinDamage, Is.EqualTo(6));
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithLeftHandMeleeAttacks.MaxDamage, Is.EqualTo(12));
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithRangedAttacks.MinDamage, Is.EqualTo(6));
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithRangedAttacks.MaxDamage, Is.EqualTo(12));
+        }
+
+        [Test]
+        public void TestDecreasePlusMinMaxPhysicalDamageWithMeleeAttacksBy_DecreasesOnlyMeleeAttacks()
+        {
+            // Arrange
+            StatsFromEquipment testCandidate = new StatsFromEquipment();
+            testCandidate.IncreasePlusMinMaxPhysicalDamageWithAttacksBy(10, 20);
+            
+            // Act
+            testCandidate.DecreasePlusMinMaxPhysicalDamageWithMeleeAttacksBy(3, 7);
+            
+            // Assert
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithRightHandMeleeAttacks.MinDamage, Is.EqualTo(7));
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithRightHandMeleeAttacks.MaxDamage, Is.EqualTo(13));
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithLeftHandMeleeAttacks.MinDamage, Is.EqualTo(7));
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithLeftHandMeleeAttacks.MaxDamage, Is.EqualTo(13));
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithRangedAttacks.MinDamage, Is.EqualTo(10));
+            Assert.That(testCandidate.PlusMinMaxPhysicalDamageWithRangedAttacks.MaxDamage, Is.EqualTo(20));
+        }
     }
 }
