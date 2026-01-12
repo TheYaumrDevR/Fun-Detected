cbf5263866af72b11930a8e2e0ea546b35a882e4
Faraon <phah.ra.on@gmail.com>
2026-01-13 00:02:11 +0100

    add localizationgateway interface

--- Assets/Scripts/org/ethasia/fundetected/technical/DictionaryBasedLocalizationGateway.cs
+++ Assets/Scripts/org/ethasia/fundetected/technical/DictionaryBasedLocalizationGateway.cs
@@ -1,13 +1,12 @@
 using System.Collections.Generic;
 
+using Org.Ethasia.Fundetected.Interactors;
+
 namespace Org.Ethasia.Fundetected.Technical
 {
     // Can be used if no proper localization is implemented
-    public class DictionaryBasedLocalizationGateway
+    public class DictionaryBasedLocalizationGateway : ILocalizationGateway
     {
-        // TODO: add interface LocalizationGateway in interactors namespace
-        // TODO: Make FunDetectedWindow use the interface
-
         private Dictionary<string, string> localizedStrings = new Dictionary<string, string>
         {
             { "inventory-window-title", "Inventory" }
