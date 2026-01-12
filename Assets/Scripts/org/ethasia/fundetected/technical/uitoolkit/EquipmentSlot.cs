dbde5dd4e3df4fcb9a16f7628e7e7c0e19c523ee
Faraon <phah.ra.on@gmail.com>
2026-01-13 00:28:20 +0100

    add UXML for equipment slots

---
+++ Assets/Scripts/org/ethasia/fundetected/technical/uitoolkit/EquipmentSlot.cs
@@ -0,0 +1,9 @@
+namespace Org.Ethasia.Fundetected.Technical.UIToolkit
+{
+    public class EquipmentSlot : VisualElement
+    {
+        private const string BACKGROUND_IMAGE_NAME = "background-image";
+        private const string OVERLAY_NAME = "overlay-element";
+        private const string ITEM_IMAGE_NAME = "item-image";
+    }
+}
