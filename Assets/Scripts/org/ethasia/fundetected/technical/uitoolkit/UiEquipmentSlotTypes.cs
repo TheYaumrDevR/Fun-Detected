aa51f152b5ca1ef5bffddf7f04dd8feb3d22a7fc
Faraon <phah.ra.on@gmail.com>
2026-01-13 23:37:49 +0100

    add EquipmentSlot UI element render background based on slot type

---
+++ Assets/Scripts/org/ethasia/fundetected/technical/uitoolkit/UiEquipmentSlotTypes.cs
@@ -0,0 +1,12 @@
+namespace Org.Ethasia.Fundetected.Technical.UIToolkit
+{
+    public enum UiEquipmentSlotTypes
+    {
+        LARGE,
+        BIG,
+        MEDIUM,
+        BELT,
+        JEWELRY,
+        POTION
+    }
+}
