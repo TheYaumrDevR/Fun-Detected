8d7a8b09b4debf1604bc060d2709e6d2b8fe5694
Faraon <phah.ra.on@gmail.com>
2026-02-05 00:33:14 +0100

    add border image to equipment slots

--- Assets/Scripts/org/ethasia/fundetected/technical/uitoolkit/EquipmentSlot.cs
+++ Assets/Scripts/org/ethasia/fundetected/technical/uitoolkit/EquipmentSlot.cs
@@ -10,11 +10,27 @@ namespace Org.Ethasia.Fundetected.Technical.UIToolkit
     {
         private const string OVERLAY_NAME = "overlay-element";
         private const string ITEM_IMAGE_NAME = "item-image";
+        private const string BORDER_IMAGE_NAME = "border-image";
+
         private const string BLUE_OVERLAY_CLASS_NAME = "equipment-slot-overlay-blue";
         private const string RED_OVERLAY_CLASS_NAME = "equipment-slot-overlay-red";
 
         private VisualElement overlay;
         private VisualElement itemImage;
+        private VisualElement borderImage;
+
+        private EquipmentSlotDimensions slotDimension;
+
+        [UxmlAttribute]
+        public EquipmentSlotDimensions SlotDimension 
+        { 
+            get => slotDimension;
+            set 
+            {
+                slotDimension = value;
+                UpdateBorderClass();
+            }
+        }
 
         public EquipmentSlot()
         {
@@ -23,6 +39,7 @@ namespace Org.Ethasia.Fundetected.Technical.UIToolkit
 
             overlay = this.Q<VisualElement>(OVERLAY_NAME);
             itemImage = this.Q<VisualElement>(ITEM_IMAGE_NAME);
+            borderImage = this.Q<VisualElement>(BORDER_IMAGE_NAME);
         }
 
         public void RenderEquippedItem(EquipmentSlotRenderContext renderContext)
@@ -65,6 +82,44 @@ namespace Org.Ethasia.Fundetected.Technical.UIToolkit
         {
             overlay.RemoveFromClassList(BLUE_OVERLAY_CLASS_NAME);
             overlay.RemoveFromClassList(RED_OVERLAY_CLASS_NAME);
+        }
+
+        private void UpdateBorderClass()
+        {
+            if (borderImage != null)
+            {
+                switch (slotDimension)
+                {
+                    case EquipmentSlotDimensions.EXTRA_LARGE:
+                        borderImage.AddToClassList("equipment-slot-border-large");
+                        break;
+                    case EquipmentSlotDimensions.LARGE:
+                        borderImage.AddToClassList("equipment-slot-border-medium");
+                        break;
+                    case EquipmentSlotDimensions.MEDIUM:
+                        borderImage.AddToClassList("equipment-slot-border-small");
+                        break;
+                    case EquipmentSlotDimensions.BELT:
+                        borderImage.AddToClassList("equipment-slot-border-belt");
+                        break;
+                    case EquipmentSlotDimensions.SMALL:
+                        borderImage.AddToClassList("equipment-slot-border-jewelry");
+                        break;
+                    case EquipmentSlotDimensions.POTION:
+                        borderImage.AddToClassList("equipment-slot-border-potion");
+                        break;
+                }
+            }
+        }
+
+        public enum EquipmentSlotDimensions
+        {
+            EXTRA_LARGE,
+            LARGE,
+            MEDIUM,
+            BELT,
+            SMALL,
+            POTION
         }
     }
 }
