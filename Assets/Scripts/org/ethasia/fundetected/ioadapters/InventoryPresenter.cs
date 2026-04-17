195778c0f143403ef3a0e2c53b25b4b358c836bf
Faraon <phah.ra.on@gmail.com>
2026-04-17 19:03:36 +0200

    implement InventoryPresenter

--- Assets/Scripts/org/ethasia/fundetected/ioadapters/InventoryPresenter.cs
+++ Assets/Scripts/org/ethasia/fundetected/ioadapters/InventoryPresenter.cs
@@ -1,3 +1,4 @@
+using Org.Ethasia.Fundetected.Core.Equipment;
 using Org.Ethasia.Fundetected.Interactors.Presentation;
 using Org.Ethasia.Fundetected.Ioadapters.Technical;
 
@@ -7,22 +8,40 @@ namespace Org.Ethasia.Fundetected.Ioadapters
     {
         public void ShowSwappedEquippedWeapon(string itemId, EquippedWeaponPresentationContext context)
         {
+            EquipmentSlotRenderContext equipmentRenderContext = ItemPresentationToRenderContextConverter.ConvertWeaponEquipmentSlotPresentationContext(context.ItemPresentationContext, context.WeaponPresentationContext);
+
+            PresentEquippedItem(context.SlotPosition, equipmentRenderContext);
             ShowItemOnCursor(itemId);
         }
 
         public void ShowSwappedEquippedArmor(string itemId, EquippedArmorPresentationContext context)
         {
+            EquipmentSlotRenderContext equipmentRenderContext = ItemPresentationToRenderContextConverter.ConvertArmorEquipmentSlotPresentationContext(context.ItemPresentationContext, context.ArmorPresentationContext);
+
+            PresentEquippedItem(context.SlotPosition, equipmentRenderContext);
             ShowItemOnCursor(itemId);
         }
 
         public void ShowSwappedEquippedJewelry(string itemId, EquippedJewelryPresentationContext context)
         {
+            EquipmentSlotRenderContext equipmentRenderContext = ItemPresentationToRenderContextConverter.ConvertEquipmentSlotPresentationContext(context.ItemPresentationContext);
+
+            PresentEquippedItem(context.SlotPosition, equipmentRenderContext);
             ShowItemOnCursor(itemId);
         }
 
         public void ShowSwappedEquippedRecoveryPotion(string itemId, EquippedRecoveryPotionPresentationContext context)
         {
+            EquipmentSlotRenderContext equipmentRenderContext = ItemPresentationToRenderContextConverter.ConvertRecoveryPotionEquipmentSlotPresentationContext(context.ItemPresentationContext, context.RecoveryPotionPresentationContext);
+
+            PresentEquippedItem(context.SlotPosition, equipmentRenderContext);
             ShowItemOnCursor(itemId);
+        }
+
+        private void PresentEquippedItem(EquipmentSlotPositions slotPosition, EquipmentSlotRenderContext equipmentRenderContext)
+        {
+            IUiRenderer uiRenderer = TechnicalFactory.GetInstance().GetUiRendererInstance();
+            uiRenderer.RenderEquippedItemInInventoryWindow(slotPosition, equipmentRenderContext);
         }
 
         private void ShowItemOnCursor(string itemId)
