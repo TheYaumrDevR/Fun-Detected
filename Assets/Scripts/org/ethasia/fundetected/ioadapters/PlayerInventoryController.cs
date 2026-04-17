3271533937022ded755f78e5485999b8a9c9467f
Faraon <phah.ra.on@gmail.com>
2026-04-17 21:51:38 +0200

    add PlayerInventoryController

---
+++ Assets/Scripts/org/ethasia/fundetected/ioadapters/PlayerInventoryController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+using Org.Ethasia.Fundetected.Interactors.Items;
+
+namespace Org.Ethasia.Fundetected.Ioadapters
+{
+    public class PlayerInventoryController
+    {
+        // Create a Dictionary which maps a EquipmentSlotPositionTypes to the corresponding Interactor method
+        private Dictionary<EquipmentSlotPositionTypes, Action> slotActionBySlotPosition;
+
+        private PlayerInventoryInteractor playerInventoryInteractor;
+
+        public PlayerInventoryController()
+        {
+            playerInventoryInteractor = new PlayerInventoryInteractor();
+            slotActionBySlotPosition = new Dictionary<EquipmentSlotPositionTypes, Action>
+            {
+                { EquipmentSlotPositionTypes.MAIN_HAND, playerInventoryInteractor.SwapCursorItemWithMainHandEquipment },
+                { EquipmentSlotPositionTypes.OFF_HAND, playerInventoryInteractor.SwapCursorItemWithOffHandEquipment },
+                { EquipmentSlotPositionTypes.BELT, playerInventoryInteractor.SwapCursorItemWithBeltEquipment },
+                { EquipmentSlotPositionTypes.LEFT_RING, playerInventoryInteractor.SwapCursorItemWithLeftRingEquipment },
+                { EquipmentSlotPositionTypes.RIGHT_RING, playerInventoryInteractor.SwapCursorItemWithRightRingEquipment }
+            };
+        }
+
+        public void OnEquipmentSlotClicked(EquipmentSlotPositionTypes slotType)
+        {
+            if (slotActionBySlotPosition.TryGetValue(slotType, out Action action))
+            {
+                action.Invoke();
+            }
+        }
+    }
+}
